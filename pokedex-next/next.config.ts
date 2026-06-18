fdb731e3740e34fab8be2f1f3c5ba01a1644af36
Raffaele <raffaeleruggiero949@gmail.com>
2026-06-18 13:28:20 +0200

    q

--- pokedex-next/next.config.ts
+++ pokedex-next/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Aggiungi questa riga per permettere l'accesso dal tuo Wi-Fi locale
+  allowedDevOrigins: ['192.168.1.228'],
+  
+  // ... lascia qui eventuali altre configurazioni che avevi già
 };
 
 export default nextConfig;
