2ffe243640497da9f431ae5a6fece5bbbd484cd1
Raffaele <raffaeleruggiero949@gmail.com>
2026-06-23 12:29:53 +0200

    feat(auth): add Jwt and Passport modules for enhanced authentication

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    PrismaModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'pokedex-super-secret-key',
+      signOptions: { expiresIn: '7d' }, // Il token scadrà dopo 7 giorni
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
